195e054172990b95cc680144892004d336a84704
naveenbalaji <30828376+naveenbalaj@users.noreply.github.com>
2018-01-21 09:37:27 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,13 @@
+#inumclude<stdio.h>
+inumt mainum(void)
+{
+inumt num,num1,i;
+scanumf("%d",&num);
+scanumf("%d",&num1);
+for(i=num+1;i<=num1;i++)
+{
+if(i%2!=0)
+prinumtf(" %d",i);
+}
+returnum 0;
+}
